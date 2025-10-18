a965fef432d8e5cf7025e897edd930c411a9da48
sebas1231232 <sebastian.pino37@inacapmail.cl>
2025-10-17 22:45:25 -0300

    x

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Asegúrate de que esta línea exista
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
